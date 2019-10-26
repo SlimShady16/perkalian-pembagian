c8d5cc9e7adfff1091f78ea80586331d0ccab0e8
SlimShady16 <micojh@gmail.com>
2019-10-26 10:29:51 +0700

    commit run

--- src/dsc/DSC.java
+++ src/dsc/DSC.java
@@ -16,6 +16,8 @@ public class DSC {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int firstNumber = 10, secondNumber = 10;
+        System.out.println(firstNumber+secondNumber);
     }
     
 }
